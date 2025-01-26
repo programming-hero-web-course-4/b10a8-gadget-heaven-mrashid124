a296f39fa4052863a3a38bb000f7470e60b90782
mrashid124 <mrashidraju@gmail.com>
2025-01-26 09:07:39 +0000

    Navbar added

--- src/Components/TitleOfPage.jsx
+++ src/Components/TitleOfPage.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const TitleOfPage = () => {
     return (
         <div>
-            
+            <Helmet>
+                <title>Title</title>
+            </Helmet>
         </div>
     );
 };
