917cdf8b63d629a97acf01cccae0d61770e1d88f
mrashid124 <mrashidraju@gmail.com>
2025-01-26 00:01:31 +0000

    JSON data added

---
+++ src/Components/NavbarHomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarHomePage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NavbarHomePage;
---
+++ src/Components/TitleOfPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TitleOfPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TitleOfPage;
